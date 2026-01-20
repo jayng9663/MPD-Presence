a5434b26ef92fa84930c2172659d3696a43aa537
Jay <jayng9663@gmail.com>
2026-01-20 03:57:41 -0800

    Add config.hpp header file

---
+++ src/config.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+class Config {
+private:
+    std::string configFilePath;
+    std::map<std::string, std::string> settings;
+
+public:
+    Config(const std::string& filePath = "mpdrpc.conf");
+    bool loadConfig();
+    std::string getValue(const std::string& key) const;
+    std::string getHost() const;
+    int getPort() const;
+    std::string getPassword() const;
+    std::string getMusicFolder() const;
+};
+
+extern Config g_config;
