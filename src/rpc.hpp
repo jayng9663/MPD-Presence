22d8b092dad1f6d22aea8523771940888625ec15
Jay <jayng9663@gmail.com>
2026-01-20 03:57:55 -0800

    Add rpc.hpp header file for Discord RPC interface

---
+++ src/rpc.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <cstdint>
+#include <string>
+
+// RPC setup / teardown
+void rpc_setup();
+void rpc_initialize();
+void rpc_shutdown();
+
+// Update presence manually
+void rpc_update_presence();
+
+// Change details / state manually
+void rpc_set_details(const char* details);
+void rpc_set_state(const char* state);
+void rpc_set_starttime(int64_t starttime);
+void rpc_set_endtime(int64_t endtime);
+void rpc_set_largeimage(const std::string& url);
